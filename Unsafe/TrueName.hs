53eeef985b2b234841e4396b3c04a9b8b03fc129
mk <mirko.alic@plowtech.net>
2022-10-17 10:45:49 -0300

    [cpp] Fix template haskell

--- Unsafe/TrueName.hs
+++ Unsafe/TrueName.hs
@@ -329,7 +329,11 @@ truename = QuasiQuoter
         _ -> err $ occString occ ++ " has a strange flavour"
     makeP (name, vars) = if vars == [".."]
             then RecP name . capture VarP <$> recFields name
+#if __GLASGOW_HASKELL__ < 900
+            else return $ ConP name (map pat vars) where
+#else
             else return $ ConP name [] (map pat vars) where
+#endif
         pat n = case n of
             "_" -> WildP
             '!' : ns -> BangP (pat ns)
